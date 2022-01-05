8ab684fbc32b983ae05b61821b406ef673fb0e2a
Angelica <Angelica137@users.noreply.github.com>
2022-01-05 21:19:43 +0000

    delete obsolete

--- tests/test_perfect_numbers.py
+++ tests/test_perfect_numbers.py
@@ -1,9 +1,5 @@
 from scripts.perfect_numbers import perfect_numbers
 
 
-def test_perfect_numbers_1_returns_1():
-    assert perfect_numbers(1) == 1
-
-
 def test_perfect_numbers_15_returns_15sfactors():
     assert perfect_numbers(15) == [1, 3, 5]
