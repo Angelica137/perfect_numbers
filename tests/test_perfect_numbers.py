61190052c1355bfcf566ec63fecbbf694d2cda49
Angelica <Angelica137@users.noreply.github.com>
2022-01-06 19:23:39 +0000

    /perfect_numers returns sum of factors, red

--- tests/test_perfect_numbers.py
+++ tests/test_perfect_numbers.py
@@ -2,4 +2,4 @@ from scripts.perfect_numbers import perfect_numbers
 
 
 def test_perfect_numbers_15_returns_15sfactors():
-    assert perfect_numbers(15) == [1, 3, 5]
+    assert perfect_numbers(15) == 9
