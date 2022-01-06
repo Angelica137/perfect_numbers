5be3a9354e825cd088bbe9cd689ebb3225f11775
Angelica <Angelica137@users.noreply.github.com>
2022-01-06 19:36:39 +0000

    .nics_cat(15) returns deficient, red

--- tests/test_perfect_numbers.py
+++ tests/test_perfect_numbers.py
@@ -3,3 +3,7 @@ from scripts.perfect_numbers import perfect_numbers
 
 def test_perfect_numbers_15_returns_15sfactors():
     assert perfect_numbers(15) == 9
+
+
+def test_nics_cat_15_deficient():
+    assert nics_cat(15) == "Deficient"
