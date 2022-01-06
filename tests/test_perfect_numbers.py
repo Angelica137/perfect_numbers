63cfa8a09019765739ce30d3163008200646c0dd
Angelica <Angelica137@users.noreply.github.com>
2022-01-06 20:14:25 +0000

    delete obsolete

--- tests/test_perfect_numbers.py
+++ tests/test_perfect_numbers.py
@@ -9,13 +9,9 @@ def test_perfect_numbers_15_returns_15sfactors():
     assert aliquot_sum(15) == 9
 
 
-def test_nics_cat_15_deficient():
-    assert nics_cat(15) == "Deficient"
-
-
 def test_nics_cat_6_perfect():
     assert nics_cat(6) == "Perfect"
 
 
-def test_nics_cat_12_abundant():
-    assert nics_cat(12) == "Abundant"
+def test_nics_cat_28_perfect():
+    assert nics_cat(28) == "Perfect"
