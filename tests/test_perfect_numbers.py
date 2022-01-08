95dd7ef7874d733056418056068b47debf700df8
Angelica <Angelica137@users.noreply.github.com>
2022-01-08 22:16:00 +0000

    clear up test file

--- tests/test_perfect_numbers.py
+++ tests/test_perfect_numbers.py
@@ -30,16 +30,3 @@ def test_nics_cat_24_abundant():
 def test_nics_cat_0_exception():
     with pytest.raises(ValueError, match=r"Classification is only possible for positive integers."):
         nics_cat(0)
-
-
-'''
-with self.assertRaises(ValueError) as err:
-    classify(0)
-self.assertEqual(type(err.exception), ValueError)
-self.assertEqual(
-    err.exception.args[0],
-
-
-def test_nics_cat_neg1_exception():
-    assert nics_cat(-1) == ValueError
-'''
