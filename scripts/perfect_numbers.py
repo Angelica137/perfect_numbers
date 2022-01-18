a647851a6c1bb026352619f5cf0356acd97b3a9a
Angelica <Angelica137@users.noreply.github.com>
2022-01-18 18:15:50 +0000

    delete obsolete

--- scripts/perfect_numbers.py
+++ scripts/perfect_numbers.py
@@ -11,35 +11,3 @@ def nics_cat(n: int) -> str:
     for label, op in operators:
         if op(factors, n):
             return label
-    if factors < n:
-        return "Deficient"
-    if factors == n:
-        return "Perfect"
-    return "Abundant"
-
-
-operators = [operator.eq, operator.lt, operator.gt]
-labels = ["=", "<", ">"]
-
-
-def get_comparison_operator(a, b):
-    """
-    A function that returns an operator string based on the relation between
-    the supplied two numbers.
-    """
-    for op, label in zip(operators, labels):
-        if op(b, a):
-            return label
-    return '?'
-
-
-print("hello")
-print(get_comparison_operator(2, 6))  # returns '<'
-
-'''
-ops = [("perfect", operator.eq), ("deficient", operator.lt), ("abundant", operator.gt)]
-for label, op in ops: ...
-# or
-ops = {"perfect": operator.eq, "deficient": operator.lt, "abundant": operator.gt}
-for label, op in ops.items(): ...
-'''
